bd6ad6990ff231fc802f268654e413441b54ee17
hazem osama ibrahim <hazem3500@gmail.com>
2019-03-08 14:30:37 +0200

    added signing functionality for transactions

--- classes/Transaction.mjs
+++ classes/Transaction.mjs
@@ -1,7 +1,34 @@
+import SHA256 from 'crypto-js/sha256';
+import elliptic from 'elliptic';
+
+const ec = new elliptic.ec('secp256k1');
+
 export default class Transaction {
     constructor({ fromAddress = null, toAddress = null, amount = 0 }) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signingKey) {
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('Transaction not allowed!');
+        }
+        const hash = this.calculateHash();
+        this.signature = signingKey.sign(hash, 'base64').toDER('hex');
+    }
+
+    isValid() {
+        if (this.fromAddress === null) return true;
+        if (!this.signature) {
+            throw new Error('Transaction isn\'t signed!');
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature);
     }
 }
